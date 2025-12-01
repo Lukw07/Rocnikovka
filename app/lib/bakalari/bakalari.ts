4c47a47e3787dd8c3b217b8821300f4b6f748061
Lukw07 <kry.tuma@gmail.com>
2025-12-01 12:55:37 +0100

    fix(bakalari): add User-Agent header to login request

--- app/lib/bakalari/bakalari.ts
+++ app/lib/bakalari/bakalari.ts
@@ -134,6 +134,7 @@ export const loginToBakalari = async (
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
             },
             body: `client_id=ANDR&grant_type=password&username=${encodeURIComponent(
                 username
